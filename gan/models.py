0f1d571cd6bbc83f56ad5af4b002bdf387d1aac7
sanghunKang <sanghunkang.dev@gmail.com>
2017-08-26 19:11:14 +0900

    fun fun

--- gan/models.py
+++ gan/models.py
@@ -76,14 +76,30 @@ class SampleGenerator(BaseModel):
 class SampleDiscriminator(BaseModel):
 	def create_model(self, input_size, **unused_params):
 		h1_size = 128
-		self.D_W1 = tf.Variable(xavier_init([input_size, h1_size]), name='d/w1')
+		self.D_W_conv11 = tf.Variable(xavier_init([3, 3, 1, 10]), name='D_W_conv11')
+		self.D_W_conv12 = tf.Variable(xavier_init([3, 3, 10, 10]), name='D_W_conv12')
+		
+		self.D_W1 = tf.Variable(xavier_init([2500, h1_size]), name='d/w1')
 		self.D_b1 = tf.Variable(tf.zeros(shape=[h1_size]), name='d/b1')
 
 		self.D_W2 = tf.Variable(xavier_init([h1_size, 1]), name='d/w2')
 		self.D_b2 = tf.Variable(tf.zeros(shape=[1]), name='d/b2')
 
+
 	def run_model(self, model_input, is_training=True, **unused_params):
 		net = model_input
+
+		net = tf.reshape(model_input, shape=[-1, 50, 50, 1])
+		net = tf.nn.conv2d(net, self.D_W_conv11, strides=[1, 1, 1, 1], padding='SAME')
+		net = tf.nn.relu(net)
+		net = tf.nn.conv2d(net, self.D_W_conv12, strides=[1, 1, 1, 1], padding='SAME')
+		net = tf.nn.relu(net)
+		net = tf.nn.max_pool(net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+		
+		print(net.get_shape())
+		net = tf.reshape(net, [-1, 2500])
+		print(net.get_shape())
+
 
 		net = slim.dropout(net, 0.75, is_training=is_training, scope='d/dropout_fc1')
 		net = tf.nn.relu(tf.matmul(net, self.D_W1) + self.D_b1)
